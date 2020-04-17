0600974f5423d14ec0784b331f9ba8b5b3ece452
shanmuga2414 <shanmuga.hdb@gmail.com>
2020-04-17 17:33:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import L from 'leaflet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    const center = L.bounds([1.56073, 104.11475], [1.16, 103.502]).getCenter();
+    const map = L.map('mapdiv').setView([center.x, center.y], 12);
+
+    const basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
+      detectRetina: true,
+      maxZoom: 18,
+      minZoom: 11
+    });
+
+    map.setMaxBounds([[1.56073, 104.1147], [1.16, 103.502]]);
+
+    basemap.addTo(map);
+    getLocation();
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      }
+    }
+
+    function showPosition(position) {
+      new L.Marker([position.coords.latitude, position.coords.longitude], { bounceOnAdd: false }).addTo(map);
+      const popup = L.popup()
+        .setLatLng([position.coords.latitude, position.coords.longitude])
+        .setContent('You are here!')
+        .openOn(map);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+
+        </header>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id='mapdiv'></div>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
